6a6fd90205a1e8cf48de30bdd2ec0719bd934474
Harrison Huston <hhuston@stevens.edu>
2024-11-15 11:48:46 -0500

    Update

--- main.py
+++ main.py
@@ -1,69 +1,83 @@
+from decimal import Decimal
 import re
 import os
 import psycopg2
-import tabulate
 from dotenv import dotenv_values
 
-RED   = "\033[1;31m"  
-BLUE  = "\033[1;34m"
-CYAN  = "\033[1;36m"
-GREEN = "\033[0;32m"
-RESET = "\033[0;0m"
-BOLD    = "\033[;1m"
-REVERSE = "\033[;7m"
+PRINT_WIDTH = 12
 
 # Connect to PostgreSQL
 config = dotenv_values(".env")
-conn = psycopg2.connect(f"dbname={config['DBNAME']} user={config['USER']} password={config['PASSWORD']}"
-)
+conn = psycopg2.connect(database=config['DBNAME'], user=config['USER'], password=config['PASSWORD'], host=config["HOST"])
 cur = conn.cursor()
 
 # Get the answer key
 key = {}
 with open("key.txt", "r") as f:
     lines = f.readlines()
-    lines = " ".join(list(map(lambda x: x.strip(), lines)))
-    queries = re.split("\s*#+QUERY[1-5]#+\s+", lines)[1:]
+lines = " ".join(list(map(lambda x: x.strip(), lines)))
+queries = re.split("\s*#+QUERY[1-5]#+\s+", lines)[1:]
 
-    count = 1
-    for q in queries:
-        cur.execute(q)
-        key[f"Query_{count}"] =  {
-            "columns": [column.name for column in cur.description],
-            "result": [record for record in cur],
-            "print_width": len(max([column.name for column in cur.description], key=lambda col: len(col) + 3))
-        }
-        print( key[f"Query_{count}"]["print_width"])
+count = 1
+for q in queries:
+    cur.execute(q)
+    columns = ["Correct?"] + [column.name for column in cur.description]
+    key[f"Query_{count}"] =  {
+        "columns": columns,
+        "rows": [record for record in cur],
+    }
+    count += 1
 
-        for col in key[f"Query_{count}"]["columns"]:
-            print("\t", col, end="\t| ")
-        print()
-
-        count += 1
-
-exit()
+os.makedirs("results")
 
 # Check Student Answers
 for submission in os.scandir("submissions"):
     print(submission.path)
     with open(submission.path, "r") as f:
         lines = f.readlines()
-        lines = " ".join(list(map(lambda x: x.strip(), lines)))
-        queries = re.split("\s*#+QUERY[1-5]#+\s+", lines)[1:]
+        
+    lines = " ".join(list(map(lambda x: x.strip(), lines)))
+    queries = re.split("\s*#+QUERY[1-5]#+\s+", lines)
+    name, queries = queries[0], queries[1:]
 
-        count = 1
-        for q in queries:
-            expected = key[f"Query_{count}"]
-            cur.execute(q)
+    with open(submission.path.replace("submissions", "results"), "w") as f:
+            f.write(name)
 
-            i = 0
-            for record in cur:
-                if record != expected[i]:
-                    print(f"Failed Query {count}")
-                    break
-                i += 1
-            
-            count += 1
+    count = 1
+    for q in queries:
+        expected = key[f"Query_{count}"]
+        
+        cur.execute(q)
+
+        header = ""
+        for col in expected["columns"]:   
+            header += f"|{col:>{PRINT_WIDTH}}"
+        header += "\n"
+
+        i = 0
+        rows = []
+        for record in cur:
+            correct = f"|{chr(10003):>{PRINT_WIDTH}}"
+            row = f"|{1:>{PRINT_WIDTH}}"
+            if len(record) != len(expected["rows"][i]):
+                header = ["Incorrect number of columns in output\n"]
+                break
+            for j in range(len(expected["rows"][i])):
+                if record[j] != expected["rows"][i][j]:
+                    correct = f"|{'X':>{PRINT_WIDTH}}"
+                val = record[j]
+                if isinstance(record[j], Decimal):
+                    val = round(record[j], 2)
+                row += f"|{str(val):>{PRINT_WIDTH}}"
+            i += 1
+            rows += [correct + row + "\n"]
+
+        with open(submission.path.replace("submissions", "results"), "a") as f:
+            f.write(f"Query_{count}\n")
+            f.writelines(header)
+            f.writelines(rows)
+        count += 1
+    
 
 cur.close()
 conn.close()   
