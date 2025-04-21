0ca5ff8b25bea564af28bf667360cb4634934ea2
Harrison Huston <hhuston@stevens.edu>
2025-04-20 23:05:22 -0400

    Add config file for Canvas API Setup

---
+++ config.py
@@ -0,0 +1,9 @@
+API_URL = "https://sit.instructure.com/"
+
+# https://community.canvaslms.com/t5/Canvas-Basics-Guide/How-do-I-manage-API-access-tokens-in-my-user-account/ta-p/615312
+API_TOKEN = "ENTER YOUR API TOKEN HERE"
+
+# https://13kb.helpscoutdocs.com/article/551-how-to-locate-canvas-course-and-section-id
+COURSE_ID = "ENTER COURSE ID HERE"
+
+ASSIGNMENT_ID = "ENTER ASSIGNMENT ID HERE"
