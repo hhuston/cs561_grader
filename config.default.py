85f35c9f2a3bda5e10a88d614a1f41803f3981aa
Harrison Huston <hhuston@stevens.edu>
2025-04-23 15:47:53 -0400

    Create default config file

---
+++ config.default.py
@@ -0,0 +1,29 @@
+##########################
+# Canvas API Connections #
+##########################
+
+API_URL = "https://sit.instructure.com/"
+
+# https://community.canvaslms.com/t5/Canvas-Basics-Guide/How-do-I-manage-API-access-tokens-in-my-user-account/ta-p/615312
+API_TOKEN = "ENTER YOUR API TOKEN HERE"
+
+# https://13kb.helpscoutdocs.com/article/551-how-to-locate-canvas-course-and-section-id
+COURSE_ID = "ENTER COURSE ID HERE"
+
+# Same process as getting course id, navigate to assignment and check the URL
+ASSIGNMENT_ID = "ENTER ASSIGNMENT ID HERE"
+
+########################
+# Assignment Variables #
+########################
+
+BANNED_KEYWORDS = ["Coalesce", "Limit", "Row_number", "Case", "While"]
+
+###############
+# DO NOT EDIT #
+###############
+
+RED    = "\033[31m"  
+YELLOW = "\033[33m"
+GREEN  = "\033[32m"
+RESET  = "\033[0m"
