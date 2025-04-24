73c9b5c25a2ca4faf00982b5f74fabf7cfcf936c
Harrison Huston <hhuston@stevens.edu>
2025-04-23 20:34:54 -0400

    Add more descriptive anotations to the config default

--- config.default.py
+++ config.default.py
@@ -1,7 +1,21 @@
+################
+# Instructions #
+################
+
+# 1. Rename this file to config.py
+# 
+# 2. Fill in the variables in the Canvas API Connections
+#    There are instructions provided for how to get each value
+#
+# 3. Change variables that pertain to the assignment grading
+#
+# 4. Set the percentages for each part that the autograder checks
+
 ##########################
 # Canvas API Connections #
 ##########################
 
+# This is already set for the Stevens Canvas URL
 API_URL = "https://sit.instructure.com/"
 
 # https://community.canvaslms.com/t5/Canvas-Basics-Guide/How-do-I-manage-API-access-tokens-in-my-user-account/ta-p/615312
@@ -17,7 +31,25 @@ ASSIGNMENT_ID = "ENTER ASSIGNMENT ID HERE"
 # Assignment Variables #
 ########################
 
-BANNED_KEYWORDS = ["Coalesce", "Limit", "Row_number", "Case", "While"]
+# Ex: BANNED_SQL_KEYWORDS = ["Coalesce", "Limit", "Row_number", "Case", "While"]
+BANNED_SQL_KEYWORDS = ["ENTER BANNED SQL KEYWORDS HERE"]
+
+#####################
+# Grading Variables #
+#####################
+
+# Values represent percentages and must add to 100
+# Ex:
+# CORRECT_NUM_HEADERS_PERCENTAGE = 5
+# CORRECT_HEADERS_PERCENTAGE = 10
+# CORRECT_NUM_RECORDS_PERCENTAGE = 5
+# CORRECT_RECORDS_PERCENTAGE = 75
+# CORRECT_ORDER_PERCENTAGE = 5
+CORRECT_NUM_HEADERS_PERCENTAGE = 0
+CORRECT_HEADERS_PERCENTAGE = 0
+CORRECT_NUM_RECORDS_PERCENTAGE = 0
+CORRECT_RECORDS_PERCENTAGE = 0
+CORRECT_ORDER_PERCENTAGE = 0
 
 ###############
 # DO NOT EDIT #
